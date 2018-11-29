bce79fbab28b00dfb551f32db56820124631f276
aArdrey2019 <42845249+aArdrey2019@users.noreply.github.com>
2018-11-29 14:09:12 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Home</title>
+</head>
+<body>
+	<h1>Hello World!</h1>
+	<p>This is my first PHP</p>
+	<form action="process.php" method="post">
+		<input name="name" type="text" placeholder="input name"><br>
+		<input type="submit">
+	</form>	
+	
+	<?php
+	
+		$name = "name";
+		echo $name;
+	
+	?>
+</body>
+</html>
